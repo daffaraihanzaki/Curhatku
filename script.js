8d534f9259388b0e808f32d0bc6be054d150084e
Daffa Raihan <daffaraihanzaki@gmail.com>
2019-11-26 15:34:48 +0700

    first

---
+++ script.js
@@ -0,0 +1,13 @@
+
+// const btnNext = document.querySelector('.btn-next');
+// btnNext.addEventListener('click',function(){
+//     for(let asu = 1; asu <= 10; asu++){
+//         const page = document.querySelector('.page');
+//         const y = asu;
+//         page.innerHTML = y;
+//     }
+
+    
+
+    
+// });
